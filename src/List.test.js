704c3f69d679947ac637a22fc5efd134ba4a28f5
Kyler <Krenneker16@gmail.com>
2019-03-16 22:27:32 -0400

    got project working the best I could

---
+++ src/List.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+import renderer from 'react-test-renderer';
+
+describe('Card component', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<List />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+        .create(<List key={1} header="First list" card={[{ id: 'a', title: 'First card', content: 'lorem ipsum' }, { id: 'b', title: 'Second card', content: 'lorem ipsum' }]} />)
+        .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+})
